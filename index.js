fdfcf2e39e299c088760129241a2fec48e5103e2
Franklin Benjamin <franciscovibes14@gmail.com>
2022-05-19 22:43:12 -0400

    commit tree 0221c3c9680d6d23bbbac474a4939eda7bd51e50

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    const data = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    names[i] = data
+  }
+  return names;
+}
+writeCards(names);
+
+function countDown(){
+    let decrease = 10
+    while(decrease > -1){
+        console.log(decrease)
+        decrease--
+    }
+}
+countDown(4);
